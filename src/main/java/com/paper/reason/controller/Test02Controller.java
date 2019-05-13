a5c529cbf026d9bb9e57916dbdc26b796ed41bb6
栗星星 <lixingxing_lz@126.com>
2019-05-13 23:26:17 +0800

    提交代码

---
+++ src/main/java/com/paper/reason/controller/Test02Controller.java
@@ -0,0 +1,12 @@
+package com.paper.reason.controller;
+
+/**
+ * The document was created in 2019/5/13
+ *
+ * @author Lixingxing
+ */
+public class Test02Controller {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+    }
+}
