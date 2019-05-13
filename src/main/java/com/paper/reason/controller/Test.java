c84922c08616fa8b36fa17ea66d9b8cfcbf4ff8d
栗星星 <lixingxing_lz@126.com>
2019-05-13 23:30:32 +0800

    提交代码

---
+++ src/main/java/com/paper/reason/controller/Test.java
@@ -0,0 +1,13 @@
+package com.paper.reason.controller;
+
+/**
+ * The document was created in 2019/5/13
+ *
+ * @author Lixingxing
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
+
