8796ee3369753289b084a0fa8844604f64e4bd1b
栗星星 <lixingxing_lz@126.com>
2019-05-20 16:01:30 +0800

    提交代码

--- src/main/java/com/paper/reason/dao/UserInfoMapper.java
+++ src/main/java/com/paper/reason/dao/UserInfoMapper.java
@@ -1,11 +1,9 @@
 package com.paper.reason.dao;
 
 import com.paper.reason.model.UserInfo;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper
 public interface UserInfoMapper {
     int deleteByPrimaryKey(Long userSid);
 
